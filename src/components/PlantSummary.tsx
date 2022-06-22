75d7e13837bd5199ed846efe848c7519a52fc2f9
chris-ac-malone <chris.ac.malone@gmail.com>
2022-06-21 22:14:11 -0600

    Stubbed PlantSummary component. Added props to the component

--- src/components/PlantSummary.tsx
+++ src/components/PlantSummary.tsx
@@ -1,3 +1,23 @@
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import * as RNFS from 'react-native-fs';
+
+
+const PlantSummary = ({ commonName, scientificName, image }: {commonName: string; scientificName: string; image: string}) => {
+    return (
+      <View style={styles.container}>
+        <Text>{commonName}</Text>
+      </View>
+    )
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  export default PlantSummary;
